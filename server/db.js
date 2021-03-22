b866b954122ad77ce2dc4bce355d23cd38d8f293
DonHagilito <christoffer.hag@gmail.com>
2021-03-22 11:00:20 +0100

    CRUD with happy paths working

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ const addEnvelope = (name, saveAmount, total) =>{
         name: name,
         saveAmount: saveAmount,
         total: total ? total : 0,
-        id: 0
+        id: envelopes.length
     }
     envelopes.push(objectToPush);
     return envelopes[envelopes.length-1];
@@ -19,11 +19,52 @@ const addEnvelope = (name, saveAmount, total) =>{
 
 
 const getEnvelopes = () => {
-    console.log(envelopes);
     return envelopes;
+}
+
+const getEnvelopeById = (id) => {
+    const envelope = envelopes.filter(env =>{
+        return env.id === id;
+    })
+    return envelope;
+}
+
+const getIndexById = (id) => {
+    
+    for (let i=0; i<envelopes.length; i++){
+        if (envelopes[i].id === id){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+const updateEnvelopeById = (id, name, saveAmount, total) => {
+    const index = getIndexById(id);
+    envelopes[index].name = name;
+    envelopes[index].saveAmount = saveAmount;
+    envelopes[index].total = total;
+
+    return envelopes[index];
+}
+
+const deleteEnvelopeById = (id) => {
+    const index = getIndexById(id);
+
+    if (index === -1 ){
+        return false
+    }
+
+    envelopes.splice(index, 1);
+    console.log(envelopes);
+    return true;
 }
 
 module.exports = {
     addEnvelope,
-    getEnvelopes
+    getEnvelopes,
+    getEnvelopeById,
+    updateEnvelopeById,
+    deleteEnvelopeById
 };
--- server/router.js
+++ server/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const user = require('./db');
 const bodyParser = require('body-parser');
 const {
   addEnvelope,
-  getEnvelopes
+  getEnvelopes,
+  getEnvelopeById,
+  updateEnvelopeById,
+  deleteEnvelopeById
 } = require('./db.js')
 
 router.use(bodyParser.json());
@@ -27,6 +29,34 @@ router.post('/api/envelope', (req, res, next) => {
   const addedEnvelope = addEnvelope(body.name, body.saveAmount, body.total)
 
   res.send(addedEnvelope);
+})
+
+router.get('/api/envelopes/:id', (req, res, next) =>{
+  const id = parseInt(req.params.id);
+  const envelope = getEnvelopeById(id);
+
+  res.send(envelope);
+})
+
+router.put('/api/envelopes/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const body = req.body;
+
+  const updatedEnvelope = updateEnvelopeById(id, body.name, body.saveAmount, body.total);
+  res.send(updatedEnvelope);
+
+})
+
+router.delete('/api/envelopes/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+
+  const result = deleteEnvelopeById(id);
+  if (result){
+    res.status(204).send();
+  } else{
+    res.status(404).send();
+  }
+
 })
 
 
